18b9e3a265b7b83277eeaef490152c72ed72288f
sun5066 <alstjr4434@naver.com>
2022-06-24 00:46:44 +0900

    #2 add permission check

--- app/src/main/java/github/sun5066/cameraxcompose/MainActivity.kt
+++ app/src/main/java/github/sun5066/cameraxcompose/MainActivity.kt
@@ -1,19 +1,16 @@
 package github.sun5066.cameraxcompose
 
-import android.content.pm.PackageManager
+import android.Manifest
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.ManagedActivityResultLauncher
-import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
-import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.runtime.*
-import androidx.compose.ui.platform.LocalContext
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.rememberPermissionState
 import github.sun5066.cameraxcompose.camera.CameraView
@@ -29,7 +26,7 @@ class MainActivity : ComponentActivity() {
             CameraXComposeTheme {
                 // A surface container using the 'background' color from the theme
                     CheckPermission(
-                        permission = "android.Manifest.permission.CAMERA",
+                        permission = Manifest.permission.CAMERA,
                         onGranted = {
                             Surface(color = MaterialTheme.colors.background) {
                             CameraView(
@@ -49,28 +46,22 @@ class MainActivity : ComponentActivity() {
 
 @ExperimentalPermissionsApi
 @Composable
-private fun CheckPermission(
-    permission: String,
-    onDenied: @Composable (requester: () -> Unit) -> Unit = {},
-    onGranted: @Composable () -> Unit = {}
-) {
-    val context = LocalContext.current
-    var grantState by remember {
-        mutableStateOf(
-            ContextCompat.checkSelfPermission(
-                context,
-                permission
-            ) == PackageManager.PERMISSION_GRANTED
-        )
-    }
+private fun CheckPermission(permission: String, onGranted: @Composable () -> Unit = {}) {
+    val cameraPermissionState = rememberPermissionState(permission)
 
-    if (grantState) {
+    if (cameraPermissionState.hasPermission) {
         onGranted.invoke()
     } else {
-        val launcher: ManagedActivityResultLauncher<String, Boolean> =
-            rememberLauncherForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
-                grantState = isGranted
+        Column {
+            val textToShow = if (cameraPermissionState.shouldShowRationale) {
+                "The camera is important for this app. Please grant the permission."
+            } else {
+                "Camera permission required for this feature to be available. Please grant the permission"
             }
-        onDenied { launcher.launch(permission) }
+            Text(textToShow)
+            Button(onClick = { cameraPermissionState.launchPermissionRequest() }) {
+                Text("Request permission")
+            }
+        }
     }
 }
